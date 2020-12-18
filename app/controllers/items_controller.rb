745bb614fd666591d89d888c44b3054c37fafc98
ma1sa007 <74030257+ma1sa007@users.noreply.github.com>
2020-12-18 19:02:23 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,10 +31,15 @@ class ItemsController < ApplicationController
       end
     
       def update
-        # binding.pry
         item = Item.find(params[:id])
         item.update(item_params)
         redirect_to item_path
+      end
+
+      def destroy
+        item = Item.find(params[:id])
+        item.destroy
+        redirect_to root_path
       end
     
       private
