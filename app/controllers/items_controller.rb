8b12b2bb6985accbed5d1cc90c4daf03dbdba007
ma1sa007 <74030257+ma1sa007@users.noreply.github.com>
2020-12-20 10:38:09 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create,:edit]
+  before_action :authenticate_user!,only: [:show, : :update, :destroy]
     
     def index
          @items = Item.all.order("created_at DESC")
