9ea2e06b461dee5ac314f51a1f6ef54fcadb38fa
ma1sa007 <74030257+ma1sa007@users.noreply.github.com>
2020-12-20 12:22:53 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show,:update, :destroy]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
     
     def index
@@ -36,6 +36,7 @@ class ItemsController < ApplicationController
       end
 
       def destroy
+        @item.destroy
         if current_user != @item.user
          redirect_to root_path
        end
