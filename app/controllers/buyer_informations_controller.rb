05b15fe3c4c0db61980c0d9c68fd00a1ef2c283c
ma1sa007 <74030257+ma1sa007@users.noreply.github.com>
2020-12-23 18:15:44 +0900

    商品購入機能　追加３

--- app/controllers/buyer_informations_controller.rb
+++ app/controllers/buyer_informations_controller.rb
@@ -29,7 +29,7 @@ class BuyerInformationsController < ApplicationController
   private
 
   def item_params
-    params.require(:buyer_information).permit(:postal_code, :shipping_area_id, :municipalities, :building_name, :phone_number, :address, :token).merge(user_id: current_user.id, token: :token, item_id: params[:item_id])
+    params.require(:buyer_information).permit(:postal_code, :shipping_area_id, :municipalities, :building_name, :phone_number, :address, :token).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
